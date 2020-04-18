5d72ef4a1cb6c271bcdca559b8bd46dd97a13769
shuwei <wangshuwei6@gmail.com>
2020-04-18 16:29:31 +0800

    TODO

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- spreadme bootstrap encrypt
---
+++ spreadme-boot/src/test/java/org/spreadme/boot/test/main/CustomPropertySourceLocator.java
@@ -0,0 +1,8 @@
+package org.spreadme.boot.test.main;
+
+/**
+ * @description:
+ * @author shuwei.wang
+ */
+public class CustomPropertySourceLocator {
+}
